584117b385d05ff6998a558961ce3fecbb361508
Jeroen Bourgois <jeroen@jackjoe.be>
2022-05-04 19:21:15 +0200

    feat: take system tray width into account

--- dwm.c
+++ dwm.c
@@ -191,7 +191,7 @@ static void detachstack(Client *c);
 // static Monitor *dirtomon(int dir);
 static void drawbar(Monitor *m);
 static void drawbars(void);
-static int drawstatusbar(Monitor *m, int bh, char* text);
+static int drawstatusbar(Monitor *m, int bh, int stw, char* text);
 static void enternotify(XEvent *e);
 static void expose(XEvent *e);
 static void focus(Client *c);
@@ -846,7 +846,7 @@ dirtomon(int dir)
 } */
 
 int
-drawstatusbar(Monitor *m, int bh, char* stext) {
+drawstatusbar(Monitor *m, int bh, int stw, char* stext) {
 	int ret, i, w, x, len;
 	short isCode = 0;
 	char *text;
@@ -884,6 +884,7 @@ drawstatusbar(Monitor *m, int bh, char* stext) {
 	text = p;
 
 	w += 2; /* 1px padding on both sides */
+	w += stw; /* add system tray width */
 	ret = x = m->ww - w;
 
 	drw_setscheme(drw, scheme[LENGTH(colors)]);
@@ -970,7 +971,7 @@ drawbar(Monitor *m)
 
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
-		tw = m->ww - drawstatusbar(m, bh, stext);
+		tw = m->ww - drawstatusbar(m, bh, stw, stext);
 	}
 
 	resizebarwin(m);
